d0dfd52913a165e76b0849eb6a4c2d103a7680af
Daniel Bedo <daniel.bedo@gutefrage.net>
2015-02-05 18:18:12 +0100

    bumped versions

--- project/Build.scala
+++ project/Build.scala
@@ -3,12 +3,10 @@ import Keys._
 import spray.revolver.RevolverPlugin._
 import org.scalastyle.sbt.ScalastylePlugin
  
-object Resolvers {
-}
  
 object Dependencies {
    val appDependencies = Seq(
-      "org.specs2" %% "specs2" % "2.3.10" % "test"
+      "org.specs2" %% "specs2" % "2.4.15" % "test"
    )
 }
  
@@ -17,16 +15,22 @@ object BuildSettings {
    val buildOrganization = "johnmurray.io"
    val appName = "CHANGE_ME"
    val buildVersion = "0.0.1-SNAPSHOT"
-   val buildScalaVersion = "2.11.1"
+   val buildScalaVersion = "2.11.4"
    val buildScalaOptions = Seq("-unchecked", "-deprecation", "-encoding", "utf8")
- 
-   import Resolvers._
+   val buildResolvers = Seq[sbt.Resolver](
+    "Typesafe Repo"             at "http://repo.typesafe.com/typesafe/releases/",
+    "Sonatype Snapshots"        at "http://oss.sonatype.org/content/repositories/snapshots",
+    "Sonatype Releases"         at "http://oss.sonatype.org/content/repositories/releases",
+    "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases"
+  )
+
    import Dependencies._
  
    val buildSettings = Defaults.defaultSettings ++ Seq(
       organization         := buildOrganization,
       version              := buildVersion,
       scalaVersion         := buildScalaVersion,
+      resolvers            := buildResolvers,
       libraryDependencies ++= appDependencies,
       scalacOptions        := buildScalaOptions
    ) ++ Revolver.settings ++ ScalastylePlugin.Settings
